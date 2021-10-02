0ecc19a454823303995b35cfcd97a0179bed4328
Edgar-Meza <edgar.m.dev@gmail.com>
2021-10-02 18:55:48 -0500

    Se agrego el Logo e imagen de inicio

--- js/helloWorldWind.js
+++ js/helloWorldWind.js
@@ -8,7 +8,25 @@ globoWorld.addLayer(new WorldWind.CompassLayer());
 globoWorld.addLayer(new WorldWind.CoordinatesDisplayLayer(globoWorld));
 globoWorld.addLayer(new WorldWind.ViewControlsLayer(globoWorld));
 
-const coordenadas = [
+
+var satelitesData = [
+                      {
+                        lat:1.0,
+                        lon:-10.7
+                      },{
+                        lat:5.0,
+                        lon:-77.7
+                      },{
+                        lat:19.0,
+                        lon:-15.7
+                      }
+                      ,{
+                        lat:0,
+                        lon:0
+                      }
+                    ]
+
+const basuraData = [
                       {
                         lat:10.0,
                         lon:-30.7
@@ -39,85 +57,91 @@ const coordenadas = [
                       }
                     ]
 
-const satelites = [
-                      {
-                        lat:1.0,
-                        lon:-10.7
-                      },{
-                        lat:5.0,
-                        lon:-77.7
-                      },{
-                        lat:19.0,
-                        lon:-15.7
-                      }
-                    ]
-
-for(var i=0; i<coordenadas.length; i++){
-
-  var lati = coordenadas[i].lat;
-  var long = coordenadas[i].lon;
-
-  console.log(lati, long);
-
-
-  var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-  globoWorld.addLayer(placemarkLayer);
-
-  var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-
-  placemarkAttributes.imageOffset = new WorldWind.Offset(
-      WorldWind.OFFSET_FRACTION, 0.3,
-      WorldWind.OFFSET_FRACTION, 0.0);
-
-  placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-  placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-              WorldWind.OFFSET_FRACTION, 0.5,
-              WorldWind.OFFSET_FRACTION, 1.0);
-
-  placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/view/view-pitch-up-32x32.png";
-
-  var position = new WorldWind.Position(lati, long, 100.0);
-  var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
-
-  placemark.label = "Basura "+i+" ubicacion: \n" +
-      "Latitud " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-      "Longitud " + placemark.position.longitude.toPrecision(5).toString();
-  placemark.alwaysOnTop = true;
-
-  placemarkLayer.addRenderable(placemark);
+fetchData = () =>{
+  let config = {
+            mode:'no-cors',
+        }
+  fetch('https://www.space-track.org/basicspacedata/query/class/satcat/orderby/PERIOD%20asc/emptyresult/show', config)
+  .then(res => console.log(res))
+  .then(data =>{
+    console.log(data);
+  })
 }
 
-for(var i=0; i<satelites.length; i++){
 
-  var lati = satelites[i].lat;
-  var long = satelites[i].lon;
+basura = (basuraData) =>{
+  for(var i=0; i<basuraData.length; i++){
 
-  console.log(lati, long);
+    var lati = basuraData[i].lat;
+    var long = basuraData[i].lon;
+
+    console.log(lati, long);
 
 
-  var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-  globoWorld.addLayer(placemarkLayer);
+    var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+    globoWorld.addLayer(placemarkLayer);
 
-  var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
 
-  placemarkAttributes.imageOffset = new WorldWind.Offset(
-      WorldWind.OFFSET_FRACTION, 0.3,
-      WorldWind.OFFSET_FRACTION, 0.0);
+    placemarkAttributes.imageOffset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION, 0.3,
+        WorldWind.OFFSET_FRACTION, 0.0);
 
-  placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-  placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-              WorldWind.OFFSET_FRACTION, 0.5,
-              WorldWind.OFFSET_FRACTION, 1.0);
+    placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+    placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+                WorldWind.OFFSET_FRACTION, 0.5,
+                WorldWind.OFFSET_FRACTION, 1.0);
 
-  placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/crosshair.png";
+    placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/view/view-pitch-up-32x32.png";
 
-  var position = new WorldWind.Position(lati, long, 100.0);
-  var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+    var position = new WorldWind.Position(lati, long, 64.66);
+    var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
 
-  placemark.label = "satelite "+i+" ubicacion: \n" +
-      "Latitud " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
-      "Longitud " + placemark.position.longitude.toPrecision(5).toString();
-  placemark.alwaysOnTop = true;
+    placemark.label = "Basura "+i+" ubicacion: \n" +
+        "Latitud " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
+        "Longitud " + placemark.position.longitude.toPrecision(5).toString();
+    placemark.alwaysOnTop = true;
 
-  placemarkLayer.addRenderable(placemark);
+    placemarkLayer.addRenderable(placemark);
+  }
 }
+
+satelites = (satelitesData) =>{
+  fetchData();
+  for(var i=0; i<satelitesData.length; i++){
+
+    var lati = satelitesData[i].lat;
+    var long = satelitesData[i].lon;
+
+    console.log(lati, long);
+
+
+    var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+    globoWorld.addLayer(placemarkLayer);
+
+    var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+    placemarkAttributes.imageOffset = new WorldWind.Offset(
+        WorldWind.OFFSET_FRACTION, 0.3,
+        WorldWind.OFFSET_FRACTION, 0.0);
+
+    placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+    placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+                WorldWind.OFFSET_FRACTION, 0.5,
+                WorldWind.OFFSET_FRACTION, 1.0);
+
+    placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/crosshair.png";
+
+    var position = new WorldWind.Position(lati, long, 100.0);
+    var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+    placemark.label = "satelite "+i+" ubicacion: \n" +
+        "Latitud " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
+        "Longitud " + placemark.position.longitude.toPrecision(5).toString();
+    placemark.alwaysOnTop = true;
+
+    placemarkLayer.addRenderable(placemark);
+  }
+}
+
+satelites(satelitesData);

dca7d5450b017d9e1c7b03a2ee51633c26fd71e3
Edgar-Meza <edgar.m.dev@gmail.com>
2021-10-02 15:47:54 -0500

    Se agregaron puntos de prueba la imagen del globo terraque

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,10 @@
-start = () =>{
-  var home, world;
-  home = document.getElementById('home');
-  world = document.getElementById('world');
-
-  home.style.display = 'none';
-  world.style.visibility = 'initial';
-}
+var planet = planetaryjs.planet();
+// You can remove this statement if `world-110m.json`
+// is in the same path as the HTML page:
+planet.loadPlugin(planetaryjs.plugins.earth({
+  topojson: { file: 'http/path/to/world-110m.json' }
+}));
+// Make the planet fit well in its canvas
+planet.projection.scale(250).translate([250, 250]);
+var canvas = document.getElementById('globe');
+planet.draw(canvas);
--- js/helloWorldWind.js
+++ js/helloWorldWind.js
@@ -5,5 +5,71 @@ globoWorld.addLayer(new WorldWind.BMNGLandsatLayer());
 
 
 globoWorld.addLayer(new WorldWind.CompassLayer());
-globoWorld.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-globoWorld.addLayer(new WorldWind.ViewControlsLayer(wwd));
+globoWorld.addLayer(new WorldWind.CoordinatesDisplayLayer(globoWorld));
+globoWorld.addLayer(new WorldWind.ViewControlsLayer(globoWorld));
+
+const coordenadas = [
+                      {
+                        lat:10.0,
+                        lon:-30.7
+                      },
+                      {
+                        lat:60.1,
+                        lon:10.7
+                      },
+                      {
+                        lat:150.1,
+                        lon:-130.7
+                      },
+                      {
+                        lat:160.1,
+                        lon:80.7
+                      },
+                      {
+                        lat:166.1,
+                        lon:-99.4
+                      },
+                      {
+                        lat:136.1,
+                        lon:-39.4
+                      },
+                      {
+                        lat:46.1,
+                        lon:-19.4
+                      }
+                    ]
+
+for(var i=0; i<coordenadas.length; i++){
+
+  var lati = coordenadas[i].lat;
+  var long = coordenadas[i].lon;
+
+  console.log(lati, long);
+
+
+  var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+  globoWorld.addLayer(placemarkLayer);
+
+  var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+
+  placemarkAttributes.imageOffset = new WorldWind.Offset(
+      WorldWind.OFFSET_FRACTION, 0.3,
+      WorldWind.OFFSET_FRACTION, 0.0);
+
+  placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+  placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+              WorldWind.OFFSET_FRACTION, 0.5,
+              WorldWind.OFFSET_FRACTION, 1.0);
+
+  placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/view/view-pitch-up-32x32.png";
+
+  var position = new WorldWind.Position(lati, long, 100.0);
+  var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+
+  placemark.label = "Basura "+i+" ubicacion: \n" +
+      "Latitud " + placemark.position.latitude.toPrecision(4).toString() + "\n" +
+      "Longitud " + placemark.position.longitude.toPrecision(5).toString();
+  placemark.alwaysOnTop = true;
+
+  placemarkLayer.addRenderable(placemark);
+}
